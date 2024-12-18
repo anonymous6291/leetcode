ca1bfe596d9846f4126abaf9748597c89c506837
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2024-12-18 16:29:45 +0530

    Create 1475.java

---
+++ 1475.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] finalPrices(int[] p) {
+        int n = p.length;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (p[j] <= p[i]) {
+                    p[i] -= p[j];
+                    break;
+                }
+        return p;
+    }
+}
