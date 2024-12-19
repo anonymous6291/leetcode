be266821d90447d9ef8a6a601152e254fa77ddfe
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2024-12-19 11:16:49 +0530

    Create 503.java

---
+++ 503.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int s[] = new int[20005];
+        int n = nums.length, p = Integer.MIN_VALUE, ind = 10002;
+        for (int i = 0; i < n; ++i)
+            if (nums[i] > p)
+                s[ind++] = p = nums[i];
+        p = 10002;
+        for (int i = n - 1; i >= 0; --i) {
+            while (p < ind && s[p] <= nums[i])
+                ++p;
+            int t = nums[i];
+            if (p == ind)
+                nums[i] = -1;
+            else
+                nums[i] = s[p];
+            s[--p] = t;
+        }
+        return nums;
+    }
+}
