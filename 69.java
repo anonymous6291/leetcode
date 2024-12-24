6ff6e9ee01e0c90b8389232e89cf38361a15ccb0
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2024-12-24 21:59:52 +0530

    Create 69.java

---
+++ 69.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int s = 0, e = 46340;
+        while (s < e) {
+            int m = (s + e) >> 1;
+            int t = m * m;
+            if (t == x)
+                return m;
+            if (t < x)
+                s = m + 1;
+            else
+                e = m - 1;
+        }
+        if (s * s > x)
+            return s - 1;
+        return s;
+    }
+}
