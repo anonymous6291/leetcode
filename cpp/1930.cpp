ca0e34abcc52445a81a48572ee55a76ec0d3b097
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-04 11:04:53 +0530

    Create 1930.cpp

---
+++ cpp/1930.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int find(int* c, int s, int n) {
+        int e = n - 1;
+        while (e > s + 1 && c[e] != c[s])
+            --e;
+        if (s + 1 == e)
+            return 0;
+        bool un[26] = {false};
+        int count = 0;
+        for (int i = s + 1; i < e; i++)
+            if (!un[c[i]]) {
+                un[c[i]] = true;
+                ++count;
+            }
+        return count;
+    }
+
+public:
+    int countPalindromicSubsequence(string st) {
+        int count = 0, s = 0, n = st.length();
+        int* c = (int*)malloc(sizeof(int) * n);
+        for (int i = 0; i < n; i++)
+            c[i] = st[i] - 'a';
+        bool f[26] = {false};
+        while (s < n - 2) {
+            f[c[s]] = true;
+            count += find(c, s, n);
+            while (s < n - 2 && f[c[s]])
+                ++s;
+        }
+        return count;
+    }
+};
