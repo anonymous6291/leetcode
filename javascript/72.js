494bc45a76a779200ba1ada1909a3fbcc8ced401
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-05 12:25:28 +0530

    Update 72.js

--- javascript/72.js
+++ javascript/72.js
@@ -1,11 +1,13 @@
 /**
- * @param {string} word1
- * @param {string} word2
+ * @param {string} sw1
+ * @param {string} sw2
  * @return {number}
  */
 var minDistance = function (sw1, sw2) {
-    var min = function (i, j) {
-        return (i < j) ? i : j;
+    var min = function (i, j, k) {
+        if (i < j)
+            return (i < k) ? i : k;
+        return (j < k) ? j : k;
     };
     var find = function (s1, i, s2, j, dp) {
         if (i == s1.length)
@@ -18,17 +20,13 @@ var minDistance = function (sw1, sw2) {
         if (s1[i] === s2[j])
             res = find(s1, i + 1, s2, j + 1, dp);
         else
-            res = min(find(s1, i + 1, s2, j + 1, dp),
-                min(find(s1, i + 1, s2, j, dp), find(s1, i, s2, j + 1, dp))) + 1;
+            res = min(find(s1, i + 1, s2, j + 1, dp), find(s1, i + 1, s2, j, dp), find(s1, i, s2, j + 1, dp)) + 1;
         dp[i][j] = res;
         return res;
     };
-    let n1 = sw1.length, n2 = sw2.length;
-    let dp = [];
-    for (let i = 0; i < n1; i++) {
-        dp[i] = [];
-        for (let j = 0; j < n2; j++)
-            dp[i][j] = -1;
-    }
+    const n1 = sw1.length, n2 = sw2.length;
+    const dp = []
+    for (let i = 0; i < n1; i++)
+        dp.push(new Array(n2).fill(-1))
     return find(sw1, 0, sw2, 0, dp);
 };
