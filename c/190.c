ae6bf51f180c5814828b35b4c7afeb14bf1672a9
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-19 20:02:13 +0530

    Create 190.c

---
+++ c/190.c
@@ -0,0 +1,9 @@
+uint32_t reverseBits(uint32_t n) {
+    uint32_t i = 0, res = 0;
+    while (i <= 31) {
+        res = (res << 1) | (n & 1);
+        n >>= 1;
+        ++i;
+    }
+    return res;
+}
