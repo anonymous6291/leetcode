e2784d08abe2078f3c7346343f021791b275c067
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-22 16:42:29 +0530

    Create 342.java

---
+++ java/342.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0)
+            return false;
+        int z = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (n != 1)
+                    return false;
+                return (z % 2) == 0;
+            }
+            z++;
+            n >>= 1;
+        }
+        return (z % 2) == 0;
+    }
+}
