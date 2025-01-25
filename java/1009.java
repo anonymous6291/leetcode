54b50ced2059a3ff925ee20d42316d41c30bc0be
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-25 14:05:01 +0530

    Create 1009.java

---
+++ java/1009.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int res = 0, l = 1;
+        do {
+            if ((n & 1) == 0)
+                res |= l;
+            n >>= 1;
+            l <<= 1;
+        } while (n > 0);
+        return res;
+    }
+}
