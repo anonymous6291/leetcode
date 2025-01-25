ef4595fa9cb1873189d571be17affa9b67336ed6
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-25 14:06:42 +0530

    Create 1009.js

---
+++ javascript/1009.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function (n) {
+    let res = 0, l = 1;
+    do {
+        if ((n & 1) == 0)
+            res |= l;
+        n >>= 1;
+        l <<= 1;
+    } while (n > 0);
+    return res;
+};
