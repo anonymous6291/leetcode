a783f5490af6bedd542aa7adf38c2125c3e336b4
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-28 14:38:58 +0530

    Create 367.dart

---
+++ dart/367.dart
@@ -0,0 +1,16 @@
+class Solution {
+    bool isPerfectSquare(int num) {
+        int s = 1, e = 46340;
+        while (s < e) {
+            int m = (s + e) >> 1;
+            int t = m * m;
+            if (t == num)
+                return true;
+            if (t < num)
+                s = m + 1;
+            else
+                e = m - 1;
+        }
+        return (s * s == num);
+    }
+}
