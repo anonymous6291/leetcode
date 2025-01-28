ab3aefb18c8fbe901dd9c2a837206d8e52dc31e7
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-28 14:37:39 +0530

    Create 367.cpp

---
+++ cpp/367.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int s = 1, e = 46340;
+        while (s < e) {
+            int m = (s + e) / 2;
+            int t = m * m;
+            if (t == num)
+                return true;
+            if (t < num)
+                s = m + 1;
+            else
+                e = m - 1;
+        }
+        return (s * s == num);
+    }
+};
