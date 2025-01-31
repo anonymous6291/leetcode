ce3737c2b95df3f480328926e6920a1fc109575b
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-01-31 18:42:59 +0530

    Create 485.java

---
+++ java/485.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int m = 0, c = 0;
+        for (int i : nums)
+            if (i == 1)
+                c++;
+            else {
+                m = m > c ? m : c;
+                c = 0;
+            }
+        return (m > c ? m : c);
+    }
+}
