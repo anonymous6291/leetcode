6ac4a8acfb0a59d9bdc5ef1f709069eb2436a7e7
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-02-26 19:33:16 +0530

    Create 13.java

---
+++ java/13.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int get(char c) {
+        if (c == 'I')
+            return 1;
+        else if (c == 'V')
+            return 5;
+        else if (c == 'X')
+            return 10;
+        else if (c == 'L')
+            return 50;
+        else if (c == 'C')
+            return 100;
+        else if (c == 'D')
+            return 500;
+        return 1000;
+    }
+
+    public int romanToInt(String s) {
+        int n = s.length(), num = 0;
+        char p = '0';
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            num += get(c);
+            if (i != 0) {
+                if ((c == 'V' || c == 'X') && p == 'I')
+                    num = num - 2;
+                else if ((c == 'L' || c == 'C') && p == 'X')
+                    num = num - 20;
+                else if ((c == 'D' || c == 'M') && p == 'C')
+                    num = num - 200;
+            }
+            p = c;
+        }
+        return num;
+    }
+}
