bdaa4b70c38b61f95aa3a3591873c9295cf9206a
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-02-26 20:04:49 +0530

    Create 20.c

---
+++ c/20.c
@@ -0,0 +1,15 @@
+bool isValid(char* s) {
+    int n = strlen(s), i = -1;
+    char* stack = (char*)malloc(sizeof(char) * n);
+    for (int j = 0; j < n; j++)
+        if (s[j] == '(' || s[j] == '{' || s[j] == '[')
+            stack[++i] = s[j];
+        else {
+            if (i == -1 || (s[j] == ')' && stack[i] != '(') ||
+                (s[j] == '}' && stack[i] != '{') ||
+                (s[j] == ']' && stack[i] != '['))
+                return false;
+            --i;
+        }
+    return i == -1;
+}
