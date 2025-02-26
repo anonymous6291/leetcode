12de19f5810bdcae0d86c3c00c3e08a0aa497a55
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-02-26 19:17:54 +0530

    Create 9.c

---
+++ c/9.c
@@ -0,0 +1,10 @@
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+    long int v = x, r = 0;
+    while (v > 0) {
+        r = r * 10 + (v % 10);
+        v /= 10;
+    }
+    return x == r;
+}
