1e87fc53c275e6456961f3bbe36a0581aa066234
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-02-28 19:16:00 +0530

    Create 1323.java

---
+++ java/1323.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number(int num) {
+        int res = 0, p = 0, m = 1;
+        while (num > 0) {
+            res += 9 * m;
+            if (num % 10 == 6) {
+                res -= p;
+                p = 3 * m;
+            }
+            m *= 10;
+            num /= 10;
+        }
+        return res;
+    }
+}
