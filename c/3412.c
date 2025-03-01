5ff95bd3589aada40d59839852aa575cfec71868
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-03-01 11:08:13 +0530

    Create 3412.c

---
+++ c/3412.c
@@ -0,0 +1,16 @@
+long long calculateScore(char* s) {
+    int n = strlen(s), t;
+    int* stack[26];
+    for (int i = 0; i < 26; i++)
+        stack[i] = (int*)malloc(sizeof(int) * n);
+    int ind[26] = {0};
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        t = s[i] - 97;
+        if (ind[25 - t] == 0)
+            stack[t][ind[t]++] = i;
+        else
+            sum += i - stack[25 - t][--ind[25 - t]];
+    }
+    return sum;
+}
