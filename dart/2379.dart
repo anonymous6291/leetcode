cb87ef0a7fd22c283f45038430bda1da6b2d99d6
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-03-08 10:06:07 +0530

    Create 2379.dart

---
+++ dart/2379.dart
@@ -0,0 +1,18 @@
+class Solution {
+    int minimumRecolors(String blocks, int k) {
+        int s = 0, e = 0, w = 0, n = blocks.length;
+        while (e < k)
+            if (blocks[e++] == 'W')
+                w++;
+        int minw = w;
+        while (e < n) {
+            if (blocks[e++] == 'W')
+                w++;
+            if (blocks[s++] == 'W')
+                w--;
+            if (w < minw)
+                minw = w;
+        }
+        return minw;
+    }
+}
