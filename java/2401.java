04e690406595cbd9f6457074f1874eafe1efca1d
Mr. X <77231995+anonymous6291@users.noreply.github.com>
2025-03-18 14:56:25 +0530

    Create 2401.java

---
+++ java/2401.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int n = nums.length, po = 0, f = 0, r = 0, ml = 0;
+        while (f < n) {
+            while ((po & nums[f]) != 0)
+                po ^= nums[r++];
+            po |= nums[f++];
+            if (f - r > ml)
+                ml = f - r;
+        }
+        return ml;
+    }
+}
