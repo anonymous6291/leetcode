0e461acb385d25e5cca12e719c54f700493e12c0
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-03-29 18:29:53 +0530

    Create 1855.dart

---
+++ dart/1855.dart
@@ -0,0 +1,13 @@
+class Solution {
+    int maxDistance(List<int> nums1, List<int> nums2) {
+        int i = nums1.length - 1, j = nums2.length - 1, md = 0;
+        while (i >= 0) {
+            while (i <= j && nums1[i] > nums2[j])
+                j--;
+            if (j - i > md)
+                md = j - i;
+            i--;
+        }
+        return md;
+    }
+}
