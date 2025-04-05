e98b3fe2548f9ccbb93a7a384a4929d0bb29477f
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-04-05 11:27:27 +0530

    Create 1004.js

---
+++ javascript/1004.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+    let n = nums.length, c = 0, p = 0, mc = 0, i = 0;
+    while (i < n) {
+        c += nums[i++] ^ 1;
+        while (i < n && c < k)
+            c += nums[i++] ^ 1;
+        while (c > k)
+            c -= nums[p++] ^ 1;
+        if (i - p >= mc)
+            mc = i - p;
+    }
+    return mc;
+};
