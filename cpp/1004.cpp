45f47d1eb2782080b45f84d41ece7fd549d6be7f
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-04-05 11:26:51 +0530

    Create 1004.cpp

---
+++ cpp/1004.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size(), c = 0, p = 0, mc = 0, i = 0;
+        while (i < n) {
+            c += nums[i++] ^ 1;
+            while (i < n && c < k)
+                c += nums[i++] ^ 1;
+            while (c > k)
+                c -= nums[p++] ^ 1;
+            if (i - p > mc)
+                mc = i - p;
+        }
+        return mc;
+    }
+};
