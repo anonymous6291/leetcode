8d3ae5fa037a9d8d2545fa42d482b68ad8d4ad1e
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-04-05 11:26:37 +0530

    Create 1004.c

---
+++ c/1004.c
@@ -0,0 +1,13 @@
+int longestOnes(int* nums, int n, int k) {
+    int c = 0, p = 0, mc = 0, i = 0;
+    while (i < n) {
+        c += nums[i++] ^ 1;
+        while (i < n && c < k)
+            c += nums[i++] ^ 1;
+        while (c > k)
+            c -= nums[p++] ^ 1;
+        if (i - p >= mc)
+            mc = i - p;
+    }
+    return mc;
+}
