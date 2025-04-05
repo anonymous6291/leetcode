c00612906311bf35562d17b4d3fe877bdaf5b72c
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-04-05 11:53:33 +0530

    Create 1493.dart

---
+++ dart/1493.dart
@@ -0,0 +1,22 @@
+class Solution {
+    int longestSubarray(List<int> nums) {
+        int n = nums.length, p = 0, c = 0, mc = 0, i = 0;
+        while (i < n) {
+            if (nums[i++] == 0)
+                c++;
+            while (c == 2)
+                c -= nums[p++] ^ 1;
+            while (i < n) {
+                if (nums[i] == 0)
+                    if (c == 1)
+                        break;
+                    else
+                        c = 1;
+                i += 1;
+            }
+            if (i - p - 1 > mc)
+                mc = i - p - 1;
+        }
+        return mc;
+    }
+}
