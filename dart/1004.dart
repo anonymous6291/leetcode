0863a4bebaa721ef23b2c94efb974d52203eab45
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-04-05 11:27:45 +0530

    Create 1004.dart

---
+++ dart/1004.dart
@@ -0,0 +1,15 @@
+class Solution {
+    int longestOnes(List<int> nums, int k) {
+        int n = nums.length, c = 0, p = 0, mc = 0, i = 0;
+        while (i < n) {
+            c += nums[i++] ^ 1;
+            while (i < n && c < k)
+                c += nums[i++] ^ 1;
+            while (c > k)
+                c -= nums[p++] ^ 1;
+            if (i - p >= mc)
+                mc = i - p;
+        }
+        return mc;
+    }
+}
