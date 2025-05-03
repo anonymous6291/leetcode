dd22c566e6a8abf6240b8902e2d9006cf444a3a7
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-05-03 18:40:37 +0530

    Create 838.java

---
+++ java/838.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String pushDominoes(String d) {
+        char c[] = d.toCharArray();
+        int n = c.length, i = 0;
+        while (i < n) {
+            if (c[i] == 'L') {
+                int t = i - 1;
+                i++;
+                while (t >= 0 && c[t] == '.')
+                    c[t--] = 'L';
+            } else if (c[i] == 'R') {
+                int t = i + 1;
+                while (t < n && c[t] == '.')
+                    t++;
+                if (t == n || c[t] == 'R') {
+                    while (i < t)
+                        c[i++] = 'R';
+                    i = t;
+                } else if (c[t] == 'L') {
+                    int l = i + (t - i + 1) / 2;
+                    int k = i;
+                    while (i < l)
+                        c[i++] = 'R';
+                    if (((t - k) % 2) == 0)
+                        l++;
+                    while (l < t)
+                        c[l++] = 'L';
+                    i = t + 1;
+                } else
+                    i = t;
+            } else
+                i++;
+        }
+        return new String(c);
+    }
+}
