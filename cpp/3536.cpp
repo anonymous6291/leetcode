7fa92806a3ae8a065ceab178e333bf154af13f1b
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-02 19:30:30 +0530

    Create 3536.cpp

---
+++ cpp/3536.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int x = -1, y = -1;
+        while (n > 0) {
+            int t = n % 10;
+            n /= 10;
+            if (t >= x) {
+                y = x;
+                x = t;
+            } else if (t > y)
+                y = t;
+        }
+        return x * y;
+    }
+};
