197c15de1a3ab19d531d63444ac9652e9a4fd12a
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-02 19:29:41 +0530

    Create 3536.java

---
+++ java/3536.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int n) {
+        int x = -1, y = -1;
+        while (n > 0) {
+            int t = n % 10;
+            n /= 10;
+            if (t >= x) {
+                y = x;
+                x = t;
+            } else if (t > y)
+                y = t;
+        }
+        return x * y;
+    }
+}
