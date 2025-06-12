3f7bd91bc247286a76357fa7a5c682bbaf6d4f96
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-12 22:13:10 +0530

    Create 1343.java

---
+++ java/1343.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numOfSubarrays(int[] n, int k, int t) {
+        int l = n.length, p = 0, c = 0;
+        t *= k;
+        for (int i = 0; i < k; i++)
+            p += n[i];
+        if (p >= t)
+            c++;
+        for (int i = k; i < l; i++) {
+            p = p - n[i - k] + n[i];
+            if (p >= t)
+                c++;
+        }
+        return c;
+    }
+}
