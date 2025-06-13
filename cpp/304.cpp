fddf457135700b41a50f8f07ca5a591bcbb3dc4e
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-13 20:32:06 +0530

    Create 304.cpp

---
+++ cpp/304.cpp
@@ -0,0 +1,27 @@
+class NumMatrix {
+public:
+    vector<vector<int>> m;
+
+public:
+    NumMatrix(vector<vector<int>>& m) {
+        int r = m.size(), c = m[0].size();
+        for (int i = 0; i < r; i++)
+            for (int j = 1; j < c; j++)
+                m[i][j] += m[i][j - 1];
+        for (int i = 0; i < c; i++)
+            for (int j = 1; j < r; j++)
+                m[j][i] += m[j - 1][i];
+        this->m = m;
+    }
+
+    int sumRegion(int x, int y, int b, int c) {
+        return m[b][c] - (x > 0 ? m[x - 1][c] : 0) - (y > 0 ? m[b][y - 1] : 0) +
+               ((x > 0 && y > 0) ? m[x - 1][y - 1] : 0);
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
