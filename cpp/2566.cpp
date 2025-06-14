96c198bb19aafc9e643d2bdec9c5e63b4777ded7
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-14 23:05:22 +0530

    Create 2566.cpp

---
+++ cpp/2566.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minMaxDifference(int n) {
+        int t = n, l = 0;
+        while (t > 0) {
+            l++;
+            t /= 10;
+        }
+        int* d = (int*)malloc(sizeof(int) * l);
+        t = n;
+        int s = 0, e = 0, y = -1;
+        for (int i = l - 1; i >= 0; i--, t /= 10) {
+            d[i] = t % 10;
+            if (d[i] != 9)
+                y = d[i];
+        }
+        int x = d[0];
+        for (int i = 0; i < l; i++) {
+            if (x == d[i])
+                s *= 10;
+            else
+                s = s * 10 + d[i];
+            if (y == d[i])
+                e = e * 10 + 9;
+            else
+                e = e * 10 + d[i];
+        }
+        return e - s;
+    }
+};
