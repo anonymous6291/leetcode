2143bfe60b2ce93a424cf3e4ae38ada17a800aaf
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-15 20:08:04 +0530

    Create 1432.cpp

---
+++ cpp/1432.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    int len(int n) {
+        int l = 0;
+        while (n > 0) {
+            l++;
+            n /= 10;
+        }
+        return l;
+    }
+
+private:
+    int ft(vector<int>& d, int x, int y) {
+        int l = d.size();
+        for (int i = 0; i < l; i++)
+            if (d[i] != x && d[i] != y)
+                return i;
+        return -1;
+    }
+
+private:
+    int rac(vector<int>& d, int o, int n) {
+        int l = d.size(), v = 0;
+        for (int i = 0; i < l; i++)
+            v = v * 10 + (d[i] == o ? n : d[i]);
+        return v;
+    }
+
+public:
+    int maxDiff(int n) {
+        int l = len(n);
+        vector<int> d(l);
+        for (int i = l - 1, t = n; i >= 0; i--, t /= 10)
+            d[i] = t % 10;
+        int i = ft(d, -1, 9), e = i == -1 ? n : rac(d, d[i], 9);
+        i = ft(d, 0, 1);
+        return e - (i == -1 ? n : rac(d, d[i], i == 0 ? 1 : 0));
+    }
+};
