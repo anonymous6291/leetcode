87889ecf0f087176a110977cf3f6d0005c886165
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-23 19:42:26 +0530

    Create 2405.java

---
+++ java/2405.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int partitionString(String s) {
+        int n = s.length(), c = 1, f = 0;
+        for (int i = 0; i < n; i++) {
+            int x = 1 << (s.charAt(i) - 97);
+            if ((f & x) != 0) {
+                f = 0;
+                c++;
+            }
+            f |= x;
+        }
+        return c;
+    }
+}
