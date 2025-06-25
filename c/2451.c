3ae6b95721f4d4fabc59723f0cc662b5036b1dbc
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-06-25 20:44:13 +0530

    Create 2451.c

---
+++ c/2451.c
@@ -0,0 +1,32 @@
+void make(char* s, int* d) {
+    int n = strlen(s) - 1;
+    for (int i = 0; i < n; i++)
+        d[i] = s[i] - s[i + 1];
+}
+
+bool eqd(int* d1, int* d2, int n) {
+    --n;
+    while (n >= 0)
+        if (d1[n] != d2[n--])
+            return false;
+    return true;
+}
+
+char* oddString(char** w, int l) {
+    int n = strlen(w[0]) - 1;
+    int* d1 = (int*)malloc(sizeof(int) * n);
+    int* d2 = (int*)malloc(sizeof(int) * n);
+    make(w[0], d1);
+    make(w[1], d2);
+    if (!eqd(d1, d2, n)) {
+        int* d3 = (int*)malloc(sizeof(int) * n);
+        make(w[2], d3);
+        return w[eqd(d1, d3, n) ? 1 : 0];
+    }
+    for (int i = 2; i < l; i++) {
+        make(w[i], d2);
+        if (!eqd(d1, d2, n))
+            return w[i];
+    }
+    return "";
+}
