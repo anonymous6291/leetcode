c79a7ece8bf8a128a7794b3fc8ea94746b22c5f8
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-07-01 19:27:02 +0530

    Create 3330.cpp

---
+++ cpp/3330.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int possibleStringCount(string& w) {
+        int n = w.length(), c = n;
+        for (int i = 1; i < n; i++)
+            if (w[i] != w[i - 1])
+                c--;
+        return c;
+    }
+};
