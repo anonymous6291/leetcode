494e6db1d45c7ebc0e956a34d5b86c1d5c01c082
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-07-04 00:14:40 +0530

    Create 3304.cpp

---
+++ cpp/3304.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    int f(int i) {
+        int v = 1;
+        while (2 * v < i)
+            v *= 2;
+        return v;
+    }
+
+public:
+    char kthCharacter(int k) {
+        if (k == 1)
+            return 'a';
+        return (char)(kthCharacter(k - f(k)) + 1);
+    }
+};
