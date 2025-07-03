bfc3c55b60dc2e89708ec27b283bfac69839d49d
Anish Kumar Shaw <77231995+anonymous6291@users.noreply.github.com>
2025-07-04 00:14:24 +0530

    Create 3304.c

---
+++ c/3304.c
@@ -0,0 +1,12 @@
+int f(int i) {
+    int v = 1;
+    while (2 * v < i)
+        v *= 2;
+    return v;
+}
+
+char kthCharacter(int k) {
+    if (k == 1)
+        return 'a';
+    return (char)(kthCharacter(k - f(k)) + 1);
+}
